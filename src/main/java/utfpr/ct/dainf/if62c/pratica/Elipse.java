3bf1332d11bd179ee1d1f6d8c30e8b64164f2f61
xmavixx <vitholken@hotmail.com>
2015-05-10 16:36:14 -0300

    Update Elipse.java

--- src/main/java/utfpr/ct/dainf/if62c/pratica/Elipse.java
+++ src/main/java/utfpr/ct/dainf/if62c/pratica/Elipse.java
@@ -1,39 +1,45 @@
+package utfpr.ct.dainf.if62c.pratica;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package utfpr.ct.dainf.if62c.pratica;
-
-import static java.lang.Math.PI;
-import static java.lang.Math.sqrt;
-
 /**
  *
  * @author Victor
  */
-public class Elipse {
-    private double eixo1, eixo2;
+public class Elipse implements FiguraComEixos{
+    public double r;
+    public double s;
+    
+    @Override
+    public double getEixoMaior(){
+        
+        return r; 
+    }
+    @Override
+    public double getEixoMenor(){
+        return s;
+    }
     
     public Elipse(double r, double s){
-        eixo1 = r/2;
-        eixo2 = s/2;
+        this.r = r;
+        this.s = s;
+        
     }
-    
+
     public double getArea(){
-        double area;
-        
-        area = eixo1 * eixo2 * PI;
-        
-        return area;
+        double Area;
+        Area = Math.PI*getEixoMaior()*getEixoMenor();
+        return (Area);
     }
-    
+       
     public double getPerimetro(){
-        double perimetro;
-        
-        perimetro = PI * (3*(eixo1+eixo2) - sqrt((3*eixo1+eixo2)*(eixo1+3*eixo2)));
-        
-        return perimetro;
-    }
-    
+        double Perimetro;
+        Perimetro = Math.PI*((3*(getEixoMaior()+getEixoMenor()))- (Math.sqrt((3*getEixoMaior()+getEixoMenor())*(getEixoMaior()+3*getEixoMenor()))));
+        return (Perimetro);
+    }    
+
 }
